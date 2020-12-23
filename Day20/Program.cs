e4062dc75e55a27b6d743b2b48dae3d0bb5a3bd1
Leo Zlotnikov <Leo.Zlotnikov@softwire.com>
2020-12-23 04:58:38 +0000

    Day 20

---
+++ Day20/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day20
+{
+    class Program
+    {
+        private static ICollection<Tile> GetTiles()
+        {
+            var tiles = new List<Tile>();
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                if (line.Length == 0)
+                {
+                    continue;
+                }
+
+                if (line.StartsWith("Tile"))
+                {
+                    tiles.Add(new Tile
+                    {
+                        Id = int.Parse(line.Substring("Tile".Length, line.Length - "Tile".Length - 1)),
+                        Data = new List<List<char>>()
+                    });
+                    continue;
+                }
+
+                tiles[^1].Data.Add(line.ToList());
+            }
+
+            foreach (var tile in tiles)
+            {
+                tile.FindNeighbours(tiles);
+            }
+
+            return tiles;
+        }
+
+        private static long Part1(ICollection<Tile> tiles)
+        {
+            return tiles.Where(tile => tile.Neighbours.Values.Count(n => n == null) == 2)
+                .Select(t => (long) t.Id)
+                .Aggregate((x, y) => x * y);
+        }
+
+        private static List<List<Tile>> ArrangeTiles(ICollection<Tile> tiles)
+        {
+            var topLeftCorner = tiles.First(t => t.Neighbours.Values.Count(n => n == null) == 2);
+
+            while (topLeftCorner.Neighbours[Side.Up] != null || topLeftCorner.Neighbours[Side.Left] != null)
+            {
+                topLeftCorner.RotateRight();
+            }
+
+            var topRow = new List<Tile> {topLeftCorner};
+            Tile tile;
+            while ((tile = topRow[^1].Neighbours[Side.Right]) != null)
+            {
+                tile.OrientLeft(topRow[^1]);
+                topRow.Add(tile);
+            }
+
+            var rows = new List<List<Tile>> {topRow};
+            while (rows[^1][0].Neighbours[Side.Down] != null)
+            {
+                var newRow = rows[^1].Select(lastRowTile =>
+                {
+                    var newRowTile = lastRowTile.Neighbours[Side.Down];
+                    newRowTile.OrientUp(lastRowTile);
+                    return newRowTile;
+                }).ToList();
+                rows.Add(newRow);
+            }
+
+            return rows;
+        }
+
+        private static List<List<char>> ConcatTiles(List<List<Tile>> tiles)
+        {
+            return tiles.SelectMany(row =>
+                Enumerable.Range(1, row[0].Data.Count - 2).Select(i =>
+                    row.SelectMany(t => t.Data[i].Take(t.Data[i].Count - 1).Skip(1)).ToList()
+                )
+            ).ToList();
+        }
+
+        private static (bool, List<List<char>>) FindMonsters(List<List<char>> img)
+        {
+            var monster = new List<List<char>>
+            {
+                "                  # ".ToList(),
+                "#    ##    ##    ###".ToList(),
+                " #  #  #  #  #  #   ".ToList()
+            };
+
+            var found = false;
+            var image = img.Select(r => new List<char>(r)).ToList();
+
+            for (var i = 0; i < image.Count - monster.Count + 1; i++)
+            {
+                for (var j = 0; j < image[i].Count - monster[0].Count + 1; j++)
+                {
+                    var isMonster = true;
+                    for (var k = 0; k < monster.Count && isMonster; k++)
+                    {
+                        for (var l = 0; l < monster[k].Count && isMonster; l++)
+                        {
+                            if (monster[k][l] == '#' && image[i + k][j + l] != '#')
+                            {
+                                isMonster = false;
+                            }
+                        }
+                    }
+
+                    if (isMonster)
+                    {
+                        found = true;
+                        for (var k = 0; k < monster.Count; k++)
+                        {
+                            for (var l = 0; l < monster[k].Count; l++)
+                            {
+                                if (monster[k][l] == '#')
+                                {
+                                    image[i + k][j + l] = 'O';
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return (found, image);
+        }
+
+        private static int Part2(ICollection<Tile> tiles)
+        {
+            var rows = ArrangeTiles(tiles);
+            var image = new Tile
+            {
+                Data = rows.SelectMany(row =>
+                    Enumerable.Range(1, row[0].Data.Count - 2).Select(i =>
+                        row.SelectMany(t => t.Data[i].Take(t.Data[i].Count - 1).Skip(1)).ToList()
+                    )
+                ).ToList()
+            };
+
+            var (found, imageWithMonsters) = FindMonsters(image.Data);
+            for (var i = 0; i < 3 && !found; i++)
+            {
+                image.RotateRight();
+                (found, imageWithMonsters) = FindMonsters(image.Data);
+            }
+
+            if (!found)
+            {
+                image.RotateRight();
+                image.FlipUpDown();
+                (found, imageWithMonsters) = FindMonsters(image.Data);
+            }
+
+            for (var i = 0; i < 3 && !found; i++)
+            {
+                image.RotateRight();
+                (found, imageWithMonsters) = FindMonsters(image.Data);
+            }
+
+            return found ? imageWithMonsters.SelectMany(row => row).Count(c => c == '#') : -1;
+        }
+
+        static void Main(string[] args)
+        {
+            var tiles = GetTiles();
+            Console.WriteLine(Part1(tiles));
+            Console.WriteLine(Part2(tiles));
+        }
+    }
+}
---
+++ Day20/Tile.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Day20
+{
+    public enum Side
+    {
+        Up,
+        Down,
+        Left,
+        Right
+    }
+
+    public class Tile
+    {
+        private static readonly IReadOnlyList<Side> AllSides = new List<Side>
+            {Side.Up, Side.Down, Side.Left, Side.Right};
+
+        public int Id;
+        public List<List<char>> Data;
+
+        public Dictionary<Side, Tile> Neighbours = AllSides.ToDictionary(side => side, _ => (Tile) null);
+
+        private static int GetSideCode(IEnumerable<char> side)
+        {
+            return side.Aggregate(0, (code, c) => code << 1 | (c == '#' ? 1 : 0));
+        }
+
+        private (int, int) GetSide(Side side)
+        {
+            var sideData = (side switch
+            {
+                Side.Up => Data.First(),
+                Side.Down => Data.Last(),
+                Side.Left => Data.Select(r => r.First()),
+                Side.Right => Data.Select(r => r.Last()),
+                _ => null
+            })?.ToArray();
+            return (GetSideCode(sideData), GetSideCode(sideData?.Reverse()));
+        }
+
+        public void FindNeighbours(ICollection<Tile> allTiles)
+        {
+            Neighbours = new Dictionary<Side, Tile>();
+            foreach (var side in AllSides)
+            {
+                var sideCode = GetSide(side).Item1;
+                Neighbours[side] = allTiles.SingleOrDefault(otherTile =>
+                    otherTile.Id != Id
+                    && AllSides.Any(otherTileSide =>
+                    {
+                        var (otherSideCode1, otherSideCode2) =
+                            otherTile.GetSide(otherTileSide);
+                        return sideCode == otherSideCode1 ||
+                               sideCode == otherSideCode2;
+                    }));
+            }
+        }
+
+        public void RotateRight()
+        {
+            Data = Enumerable.Range(0, Data[0].Count).Select(i =>
+                Data.Select(row => row[i]).Reverse().ToList()
+            ).ToList();
+
+            var tmp = Neighbours[Side.Up];
+            Neighbours[Side.Up] = Neighbours[Side.Left];
+            Neighbours[Side.Left] = Neighbours[Side.Down];
+            Neighbours[Side.Down] = Neighbours[Side.Right];
+            Neighbours[Side.Right] = tmp;
+        }
+
+        public void RotateLeft()
+        {
+            Data = Enumerable.Range(0, Data[0].Count).Select(i =>
+                Data.Select(row => row[i]).ToList()
+            ).Reverse().ToList();
+
+            var tmp = Neighbours[Side.Up];
+            Neighbours[Side.Up] = Neighbours[Side.Right];
+            Neighbours[Side.Right] = Neighbours[Side.Down];
+            Neighbours[Side.Down] = Neighbours[Side.Left];
+            Neighbours[Side.Left] = tmp;
+        }
+
+        public void Rotate180()
+        {
+            Data.ForEach(row => row.Reverse());
+            Data.Reverse();
+
+            var tmp = Neighbours[Side.Up];
+            Neighbours[Side.Up] = Neighbours[Side.Down];
+            Neighbours[Side.Down] = tmp;
+
+            tmp = Neighbours[Side.Left];
+            Neighbours[Side.Left] = Neighbours[Side.Right];
+            Neighbours[Side.Right] = tmp;
+        }
+
+        public void FlipUpDown()
+        {
+            Data.Reverse();
+
+            var tmp = Neighbours[Side.Up];
+            Neighbours[Side.Up] = Neighbours[Side.Down];
+            Neighbours[Side.Down] = tmp;
+        }
+
+        public void FlipLeftRight()
+        {
+            Data.ForEach(row => row.Reverse());
+
+            var tmp = Neighbours[Side.Left];
+            Neighbours[Side.Left] = Neighbours[Side.Right];
+            Neighbours[Side.Right] = tmp;
+        }
+
+        public void OrientLeft(Tile otherTile)
+        {
+            var side = Neighbours.Single(neighbour => neighbour.Value == otherTile).Key;
+            switch (side)
+            {
+                case Side.Left:
+                    break;
+                case Side.Down:
+                    RotateRight();
+                    break;
+                case Side.Right:
+                    Rotate180();
+                    break;
+                case Side.Up:
+                    RotateLeft();
+                    break;
+            }
+
+            var (leftCode1, leftCode2) = GetSide(Side.Left);
+            var (otherTileRightCode, _) = otherTile.GetSide(Side.Right);
+
+            if (otherTileRightCode == leftCode2)
+            {
+                FlipUpDown();
+            }
+            else if(otherTileRightCode != leftCode1)
+            {
+                throw new InvalidDataException();
+            }
+        }
+
+        public void OrientUp(Tile otherTile)
+        {
+            var side = Neighbours.Single(neighbour => neighbour.Value == otherTile).Key;
+            switch (side)
+            {
+                case Side.Up:
+                    break;
+                case Side.Left:
+                    RotateRight();
+                    break;
+                case Side.Down:
+                    Rotate180();
+                    break;
+                case Side.Right:
+                    RotateLeft();
+                    break;
+            }
+
+            var (upCode1, upCode2) = GetSide(Side.Up);
+            var (otherTileDownCode, _) = otherTile.GetSide(Side.Down);
+
+            if (otherTileDownCode == upCode2)
+            {
+                FlipLeftRight();
+            }
+            else if(otherTileDownCode != upCode1)
+            {
+                throw new Exception();
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join("\n", Data.Select(row => new string(row.ToArray())));
+        }
+    }
+}
