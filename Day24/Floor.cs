f9b1764c82131eb779fe56e8f40904999c4f2a3f
Leo Zlotnikov <Leo.Zlotnikov@softwire.com>
2020-12-24 08:38:12 +0000

    Day 24

---
+++ Day24/Floor.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Day24
+{
+    public class Floor
+    {
+        public HashSet<(int, int)> BlackTiles;
+
+        private static readonly ICollection<(int dx, int dy)> Offsets = new[]
+        {
+            (1, 0), (0, 1), (-1, 1), (-1, 0), (0, -1), (1, -1)
+        };
+
+        public Floor(ICollection<(int, int)> blackTiles)
+        {
+            BlackTiles = new HashSet<(int, int)>(blackTiles);
+        }
+
+        private HashSet<(int, int)> GetUpdateRegion()
+        {
+            var region = new HashSet<(int, int)>();
+            foreach (var (x, y) in BlackTiles)
+            {
+                region.Add((x, y));
+                foreach (var (dx, dy) in Offsets)
+                {
+                    region.Add((x + dx, y + dy));
+                }
+            }
+
+            return region;
+        }
+
+        private int CountBlackNeighbours((int x, int y) coord)
+        {
+            var (x, y) = coord;
+            return Offsets.Count(offset => BlackTiles.Contains((x + offset.dx, y + offset.dy)));
+        }
+
+        public void Step()
+        {
+            var newTiles = new HashSet<(int, int)>();
+            var coords = GetUpdateRegion();
+
+            foreach (var coord in coords)
+            {
+                var neighbours = CountBlackNeighbours(coord);
+                var isBlack = BlackTiles.Contains(coord);
+                if (isBlack && (neighbours == 1 || neighbours == 2)
+                    || !isBlack && neighbours == 2)
+                {
+                    newTiles.Add(coord);
+                }
+            }
+
+            BlackTiles = newTiles;
+        }
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Day24
+{
+    class Program
+    {
+        private static Floor ReadTiles()
+        {
+            var tiles = new HashSet<(int, int)>();
+            foreach (var line in File.ReadLines("input.txt"))
+            {
+                var x = 0;
+                var y = 0;
+                for (var i = 0; i < line.Length; i++)
+                {
+                    switch (line[i])
+                    {
+                        case 'e':
+                            x--;
+                            break;
+                        case 'w':
+                            x++;
+                            break;
+                        case 'n':
+                            y++;
+                            if (line[++i] == 'e')
+                            {
+                                x--;
+                            }
+                            break;
+                        case 's':
+                            y--;
+                            if (line[++i] == 'w')
+                            {
+                                x++;
+                            }
+                            break;
+                    }
+                }
+
+                if (tiles.Contains((x, y)))
+                {
+                    tiles.Remove((x, y));
+                }
+                else
+                {
+                    tiles.Add((x, y));
+                }
+            }
+
+            return new Floor(tiles);
+        }
+
+        private static int Part1(Floor floor)
+        {
+            return floor.BlackTiles.Count;
+        }
+
+        private static int Part2(Floor floor)
+        {
+            for (var i = 0; i < 100; i++)
+            {
+                floor.Step();
+            }
+
+            return floor.BlackTiles.Count;
+        }
+
+        public static void Main(string[] args)
+        {
+            var floor = ReadTiles();
+            Console.WriteLine(Part1(floor));
+            Console.WriteLine(Part2(floor));
+        }
+    }
+}
